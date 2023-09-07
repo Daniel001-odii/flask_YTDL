ca059fb07992b6c7f89c9e29163cf5964b1af85e
Daniel001-odii <xenithheight@gmail.com>
2023-09-07 10:27:25 +0100

    update script

--- app.py
+++ app.py
@@ -1,53 +1,95 @@
-from flask import Flask, render_template, request, send_file, make_response, jsonify
+from flask import Flask, render_template, request, jsonify, send_file, make_response
+from flask_mail import Mail, Message
+from flask_cors import CORS
+from flask import render_template_string
+
+#for ytubefetch......
 from pytube import YouTube
 from urllib.parse import urlencode
-from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
 
+# Configuration of mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'danielsinterest@gmail.com'
+app.config['MAIL_PASSWORD'] = 'qksdojcrljuxzaso'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        url = request.form.get("url")
-        if url:
-            try:
-                yt = YouTube(url)
-                video_streams = yt.streams.filter(file_extension="mp4", progressive=True)
-                thumbnail_url = yt.thumbnail_url
-                resolutions = [{"resolution": stream.resolution, "size_mb": stream.filesize / (1024 * 1024)} for stream in video_streams]
-                return render_template("home.html", url=url, resolutions=resolutions, thumbnail_url=thumbnail_url)
-            except Exception as e:
-                error_message = "An error occurred: " + str(e)
-                return render_template("home.html", error=error_message)
-    return render_template("home.html")
 
-@app.route("/download")
-def download():
-    url = request.args.get("url")
-    resolution = request.args.get("resolution")
+#this first function is used for sending a predefined email for test....
+@app.route('/custom', methods=['GET', 'POST'])
+def custom_email():
+    msg = Message(f'{name} via Techzone support form',
+        sender='deoscomputers@gmail.com',
+        recipients=['xenithheight@gmail.com', 'deoscomputers@gmail.com']
+    )
+    name = "anon"
+    email = "anonTest@techzone.com"
+    phone = "234567890"
+    message = "this is a test email"
+    msg.body = f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    email_content = render_template_string(
+        open('email_template.html').read(),
+        name=name, email=email, phone=phone, message=message
+    )
     
-    if not url or not resolution:
-        return "URL and resolution are required parameters."
+    msg.html = email_content
+    mail.send(msg)
+    return jsonify({'message': 'Form submitted successfully'})
 
-    try:
-        yt = YouTube(url)
-        video_stream = yt.streams.filter(res=resolution, file_extension="mp4", progressive=True).first()
-        video_file = video_stream.download()
-        filename = f"{yt.title}_{resolution}.mp4"
 
-        with open(video_file, 'rb') as file:
-            video_content = file.read()
-        
-        response = make_response(video_content)
-        response.headers['Content-Type'] = 'video/mp4'
-        response.headers['Content-Disposition'] = f'attachment; filename={filename}'
-        return response
+def send_email(name, email, phone, message):
+    msg = Message( f'{name} via Techzone support form',
+        sender='deoscomputers@gmail.com',
+        recipients=['xenithheight@gmail.com']
+    )
+    email_content = render_template_string(
+        open('email_template.html').read(),
+        name=name, email=email, phone=phone, message=message
+    )
+    
+    msg.html = email_content
+    mail.send(msg)
 
-    except Exception as e:
-        return "An error occurred while downloading: " + str(e)
+
+@app.route('/', methods=['GET', 'POST'])
+def contact_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+
+        send_email(name, email, phone, message)
+
+        return jsonify({'message': 'Form submitted successfully'})
+
+    return render_template('contact_form.html')
+
+
+#the email api route is below................
+@app.route('/api/submit', methods=['POST'])
+def api_submit():
+    data = request.json
+
+    name = data.get('name')
+    email = data.get('email')
+    phone = data.get('phone')
+    message = data.get('message')
+
+    send_email(name, email, phone, message)
+
+    response = {'message': 'Form submitted successfully'}
+    return jsonify(response), 200
+
+
+
 
 
 # API endpoint to fetch video information
@@ -57,11 +99,13 @@ def api_video_info():
     if url:
         try:
             yt = YouTube(url)
+            # video_s = yt.streams.filter(file_extension="mp4")
+            # video_streams = [stream for stream in video_s if stream.includes_audio_track]
             video_streams = yt.streams.filter(file_extension="mp4")
             audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
 
             thumbnail_url = yt.thumbnail_url
-            resolutions = [{"resolution": stream.resolution, "format": stream.mime_type, "size_mb": stream.filesize / (1024 * 1024), "download_link": f"/api/download?url={url}&resolution={stream.resolution}"} for stream in video_streams]
+            resolutions = [{"resolution": stream.resolution, "hasAudio": stream.includes_audio_track, "format": stream.mime_type, "size_mb": stream.filesize / (1024 * 1024), "download_link": f"/api/download?url={url}&resolution={stream.resolution}"} for stream in video_streams]
             
             if audio_stream:
                 audio_format = audio_stream.mime_type.split("/")[-1]
@@ -91,7 +135,7 @@ def api_download():
         if resolution == "audio":
             audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
             audio_file = audio_stream.download()
-            filename = f"{yt.title}_audio.{audio_stream.mime_type.split('/')[-1]}"
+            filename = f"{yt.title}_audio.{audio_stream.mime_type.split('/')[-1]} | ytubefetch.com"
 
             with open(audio_file, 'rb') as file:
                 audio_content = file.read()
@@ -119,13 +163,4 @@ def api_download():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-# ... (previously defined routes for the web app)
 
